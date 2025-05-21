921a8ea5ce6364bdc75ecc1dd6cc7274ce190c52
PaulAllan209 <dpaulallan14@gmail.com>
2025-05-21 16:52:25 +0800

    added a utilities folder and inside it created a Default welcom markdown file static class

--- markdown_note_taking_app.Server/Repositories/Configuration/MarkdownFileConfiguration.cs
+++ markdown_note_taking_app.Server/Repositories/Configuration/MarkdownFileConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Models;
+using markdown_note_taking_app.Server.Utilities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -12,24 +13,8 @@ namespace markdown_note_taking_app.Server.Repositories.Configuration
                 new MarkdownFile
                 {
                     Id = Guid.NewGuid(),
-                    Title = "Welcome_File",
-                    FileContent = "# Welcome to Your Markdown Note Taking App\n\n" +
-                                 "This is a sample markdown file to get you started.\n\n" +
-                                 "## Features\n\n" +
-                                 "- Create and edit markdown files\n" +
-                                 "- Save your notes in the cloud\n" +
-                                 "- Collaborate with others\n\n" +
-                                 "## Markdown Syntax\n\n" +
-                                 "You can use various markdown syntax elements:\n\n" +
-                                 "**Bold text** or *italic text*\n\n" +
-                                 "- Bulleted lists\n" +
-                                 "- Like this one\n\n" +
-                                 "1. Numbered lists\n" +
-                                 "2. Are also supported\n\n" +
-                                 "```\n" +
-                                 "Code blocks too!\n" +
-                                 "```\n\n" +
-                                 "Enjoy writing!",
+                    Title = DefaultMarkdownFile.GetDefaultFileName(),
+                    FileContent = DefaultMarkdownFile.GetWelcomeFileContent(),
                     UploadDate = DateTime.Now,
                     UserId = "d8545548-6462-48c7-8149-152f8fc6406a"
                 }
---
+++ markdown_note_taking_app.Server/Utilities/DefaultMarkdownFile.cs
@@ -0,0 +1,31 @@
+﻿namespace markdown_note_taking_app.Server.Utilities
+{
+    public static class DefaultMarkdownFile
+    {
+        public static string GetWelcomeFileContent()
+        {
+            return "# Welcome to Your Markdown Note Taking App\n\n" +
+                   "This is a sample markdown file to get you started.\n\n" +
+                   "## Features\n\n" +
+                   "- Create and edit markdown files\n" +
+                   "- Save your notes in the cloud\n" +
+                   "- Collaborate with others\n\n" +
+                   "## Markdown Syntax\n\n" +
+                   "You can use various markdown syntax elements:\n\n" +
+                   "**Bold text** or *italic text*\n\n" +
+                   "- Bulleted lists\n" +
+                   "- Like this one\n\n" +
+                   "1. Numbered lists\n" +
+                   "2. Are also supported\n\n" +
+                   "```\n" +
+                   "Code blocks too!\n" +
+                   "```\n\n" +
+                   "Enjoy writing!";
+        }
+
+        public static string GetDefaultFileName()
+        {
+            return "Welcome_File";
+        }
+    }
+}
