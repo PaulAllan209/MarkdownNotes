ed7de4b4bea0c64915bf1d3a2ed5caac23fc0d33
PaulAllan209 <dpaulallan14@gmail.com>
2025-05-23 13:41:41 +0800

    improved error logging when RefreshToken fails

--- markdown_note_taking_app.Server/Exceptions/RefreshTokenBadRequest.cs
+++ markdown_note_taking_app.Server/Exceptions/RefreshTokenBadRequest.cs
@@ -1,9 +1,19 @@
-﻿namespace markdown_note_taking_app.Server.Exceptions
+﻿using markdown_note_taking_app.Server.Dto;
+
+namespace markdown_note_taking_app.Server.Exceptions
 {
     public sealed class RefreshTokenBadRequest : BadRequestException
     {
+        public TokenDto? TokenDetails { get; }
         public RefreshTokenBadRequest()
             : base("Invalid client request. The tokenDto has some invalid values.")
+        {
+        }
+
+        public RefreshTokenBadRequest(TokenDto tokenDto) 
+            : base($"Invalid client request: TokenDto values: \n" +
+                  $"AccessToken: {tokenDto.AccessToken} \n" +
+                  $"RefreshToken: {tokenDto.RefreshToken} \n")
         {
         }
     }
